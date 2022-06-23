e62faaf81f4375bc9c29395775461a3676ac67f3
zembaxa <g.zembala@gmail.com>
2022-06-23 17:17:27 +0200

    Reducer solution

--- src/components/ItemsListReducer.jsx
+++ src/components/ItemsListReducer.jsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { todos } from "../todo";
 
-export const ItemsListReducer = () => {
-  const [todoItems, setTodoItems] = React.useState(todos);
+// this is a reducer function that modifies current state
+const reducer = (state, action) => {
+  const stateItem = { ...state[action.index] };
+  switch (action.type) {
+    case "complete":
+      stateItem.status = "done";
+      state[action.index] = stateItem;
+      return [...state];
+    case "undo":
+      stateItem.status = "todo";
+      state[action.index] = stateItem;
+      return [...state];
+    default:
+      return state;
+  }
+};
 
-  // updates status of an item
-  const changeStatus = (indx) => {
-    todoItems[indx].status =
-      todoItems[indx].status === "done" ? "todo" : "done";
-    setTodoItems([...todoItems]);
+export const ItemsListReducer = () => {
+  // hook call
+  const [todoItems, dispatch] = useReducer(reducer, todos);
+
+  // handles clicking on the item
+  const onClickHandler = (index, status) => {
+    switch (status) {
+      case "todo":
+        dispatch({
+          type: "complete",
+          index: index,
+        });
+        return;
+      case "done":
+        dispatch({
+          type: "undo",
+          index: index,
+        });
+        return;
+      default:
+        return;
+    }
   };
 
   // renders list of items
   const renderList = todoItems.map((item, index) => {
     return (
       <li
-        onClick={changeStatus.bind(this, index)}
+        onClick={onClickHandler.bind(this, index, item.status)}
         className={item.status === "done" ? "done" : "todo"}
         key={index}
       >
