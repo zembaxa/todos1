447c79b2a34b1d0ee609dfdce671de674d942d32
zembaxa <g.zembala@gmail.com>
2022-06-23 18:45:00 +0200

    added solution for data flow

--- src/components/ItemsListStateInput.jsx
+++ src/components/ItemsListStateInput.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { todos } from "../todo";
+import { ItemsAdd } from './ItemsAdd';
 
 export const ItemsListStateInput = () => {
   const [todoItems, setTodoItems] = React.useState(todos);
-  const [todoInput, setTodoInput] = React.useState("");
 
   // updates status of an item
   const changeStatus = (indx) => {
@@ -11,6 +11,11 @@ export const ItemsListStateInput = () => {
       todoItems[indx].status === "done" ? "todo" : "done";
     setTodoItems([...todoItems]);
   };
+
+  const addTodoItem = (todoItem) => {
+    todoItems.push(todoItem);
+    setTodoItems([...todoItems]);
+  }
 
   // renders list of items
   const renderList = todoItems.map((item, index) => {
@@ -25,39 +30,10 @@ export const ItemsListStateInput = () => {
     );
   });
 
-  // button click handler; adds new todo item to the collection
-  const onTodoAdd = () => {
-    // construct new todo item
-    const newTodoItem = {
-      status: "todo",
-      txt: todoInput,
-    };
-
-    // push the item to the collection
-    todoItems.push(newTodoItem);
-
-    // update the component state
-    setTodoItems([...todoItems]);
-
-    // clear the input
-    setTodoInput("");
-  };
-
-  // controls the input
-  const onInputChange = (evt) => {
-    setTodoInput(evt.target.value);
-  };
-
   return (
     <>
       <h1>State</h1>
-      <div>
-        <label>New todo</label> <br />
-        <input type="text" onChange={onInputChange} value={todoInput} /> <br />
-        <button onClick={onTodoAdd} disabled={todoInput.length === 0}>
-          Add
-        </button>
-      </div>
+      <ItemsAdd addTodoFn={addTodoItem}/>
       <ul>{renderList}</ul>
     </>
   );
