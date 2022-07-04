5251217746706a76ad35304f53ee7a293d103167
zembaxa <g.zembala@gmail.com>
2022-07-04 10:39:22 +0200

    setup task

--- src/components/items/ItemsListContextReducerInput.jsx
+++ src/components/items/ItemsListContextReducerInput.jsx
@@ -1,38 +1,25 @@
 import React, { useContext } from "react";
+import { ItemsAddContext } from "./ItemsAddContext";
 import { TodosContext } from "../providers/TodosContext";
-import { ItemsAddContextReducer } from './ItemsAddContextReducer';
 
 export const ItemsListContextReducerInput = () => {
   const store = useContext(TodosContext);
-
+  
   // create new array to break the reference
   const todoItems = [...store.todoItems];
 
   // updates status of an item
-  const changeStatus = (index, status) => {
-    switch (status) {
-      case "todo":
-        store.dispatch({
-          type: "complete",
-          index: index,
-        });
-        return;
-      case "done":
-        store.dispatch({
-          type: "undo",
-          index: index,
-        });
-        return;
-      default:
-        return;
-    }
+  const changeStatus = (indx) => {
+    todoItems[indx].status =
+      todoItems[indx].status === "done" ? "todo" : "done";
+      store.setTodoItems([...todoItems]);
   };
 
   // renders list of items
   const renderList = todoItems.map((item, index) => {
     return (
       <li
-        onClick={changeStatus.bind(this, index, item.status)}
+        onClick={changeStatus.bind(this, index)}
         className={item.status === "done" ? "done" : "todo"}
         key={index}
       >
@@ -43,8 +30,8 @@ export const ItemsListContextReducerInput = () => {
 
   return (
     <>
-      <h1>State Context Reducer</h1>
-      <ItemsAddContextReducer />
+      <h1>State</h1>
+      <ItemsAddContext />
       <ul>{renderList}</ul>
     </>
   );
